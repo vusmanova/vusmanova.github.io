5de2e19cb830ae4345c8b122f1b795390bcf4750
Visolakhon Usmanova on School Machine <visolakhon.usmanova@bellevuecollege.edu>
2019-10-28 12:21:04 -0700

    Adding JavaScript Test from in class work

---
+++ javascript-tests/primitives.js
@@ -0,0 +1,29 @@
+const myString = "Learning Javascript in class.";
+
+const myFirstNumericVariable = 3;
+const myBoolean =true;
+const myNull = "null"
+let myUndefined;
+
+myUndefined = "learning more Javascript in class"; 
+
+var oldStyle = "This is the way we used to do it.";
+let newChangableStyle = " This allow you to declare a variable that can be changed.";
+const constantVariable = "This variable can not be changed. Except ...";
+
+const quotable ='He said, "Hello". That\'s all folks.';
+
+console.log(myString);
+console.log(typeof myString);
+console.log(myFirstNumericVariable);
+console.log('TYPE:', typeof myFirstNumericVariable);
+console.log(myBoolean);
+console.log('TYPE:', typeof myBoolean);
+console.log(myNull);
+console.log('TYPE:', typeof myNull);
+console.log(myUndefined);
+console.log('TYPE:', typeof myUndefined);
+console.log('Add 3 + 25', myFirstNumericVariable +25);
+console.log('Add undefined:', myUndefined +25);
+
+
