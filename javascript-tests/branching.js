5a67d9662b12c8677082061bb83a0b2cfca5cdc8
Usmanova Visolakhon <visola.usmanova@gmail.com>
2019-11-03 23:30:21 -0800

    changes to branching.js

--- javascript-tests/branching.js
+++ javascript-tests/branching.js
@@ -1,11 +1,11 @@
-let height = 5;
+let height = 25;
 
- if (height >= 5) {
+ if (height >= 25) {
      console.log('We have a big one');
 }
 
 height = 3;
-    if (height >= 5) {
+    if (height >= 25) {
         console.log('We have a big one');
     } else {
         console.log('We have a little one');
@@ -13,7 +13,7 @@ height = 3;
          
    height = 6;
 
-    if (height >= 5) {
+    if (height >= 25) {
         console.log('We have a big one');
     } else if (height >= 0) {
         console.log('We have a little one');
@@ -25,11 +25,11 @@ height = 3;
 
     switch(height) {
         case 'little':
-            console.log('Switch: a little one is smaller than five');
+            console.log('Switch: a little one is smaller than twenty five');
             break;
     
         case ('big'):
-            console.log('Switch: a big one is five or greater');
+            console.log('Switch: a big one is twenty five or greater');
             break;
     
         default:
@@ -42,7 +42,7 @@ height = 3;
         
         simpleBranchAction.onclick = (event) => {
             let height = 4;
-            if (height >= 5) {
+            if (height >= 25) {
                 simpleBranchDisplay.textContent = 'We have a big one';
             } else {
                 simpleBranchDisplay.textContent = 'We have a little one';
