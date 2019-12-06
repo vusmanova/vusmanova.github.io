26af00922a5a6782c705c6a4e79af2e0f45e57e2
Visolakhon Usmanova on School Machine <visolakhon.usmanova@bellevuecollege.edu>
2019-12-06 11:23:13 -0800

    some changes to files

--- javascript-tests/radio-choices.js
+++ javascript-tests/radio-choices.js
@@ -1,28 +1,35 @@
 window.onload = function () {
-const myForm = document.getElementById('sort-type');
+    const myForm = document.getElementById('sort-type');
 
-myForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const value = document.querySelector('input[name="sorter"]:checked').value;
-    if (value.toLowerCase() === 'green') {
-        setColor("blueBackground");
-        showChoice(value);
-        
-    } else {
-        setColor("greenBackground");
-        showChoice(value);
+    myForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const value = document.querySelector('input[name="sorter"]:checked').value;
+        if (value.toLowerCase() === 'blue') {
+            setColor("blueBackground");
+            showChoice(value);
+
+        } else {
+            setColor("greenBackground");
+            showChoice(value);
+        }
+    });
+
+    let currentClass = null;
+
+    const setColor = (className) => {
+        console.log("SET CLASS TO", className);
+        var element = document.getElementById("form-section");
+        if (currentClass) {
+            element.classList.remove(currentClass);
+        }
+        currentClass = className;
+        element.classList.add(className);
     }
-});
 
-let currentClass = null;
-
-const setColor = (className) => {
-    console.log("SET CLASS TO", className);
-    var element = document.getElementById("form-section");
-    if (currentClass) {
-        element.classList.remove(currentClass);        
-    }
-    currentClass = className;
-    element.classList.add(className);
 }
+
+const showChoice = (value) => {
+    console.log(value);
+    const userChoice = document.getElementById("user-choice");
+    userChoice.textContent= value;
 }
