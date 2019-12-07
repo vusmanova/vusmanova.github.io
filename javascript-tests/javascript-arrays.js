d50ff0305a7527e5188ce356133d4843ff61a4f2
Usmanova Visolakhon <visola.usmanova@gmail.com>
2019-12-07 14:20:02 -0800

    still correcting mistakes on this assignment

--- javascript-tests/javascript-arrays.js
+++ javascript-tests/javascript-arrays.js
@@ -10,17 +10,32 @@ window.onload = () => {
     const numbersAction = document.getElementById('numbers-action');
     const numbersDisplay = document.getElementById('numbers-display');
 
+    const languagesAction = document.getElementById('languages-action');
+    const languagesDisplay = document.getElementById('languages-display');
+
     const arrays = {
         numbers: [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5],
         languages: ["JavaScript", "HTML", "CSS", "Java", "C#", "Python", "C/C++"]
+        languagePopularity: [
+
+        ]
     };
 
     arrays.numbers.sort((a, b) => a - b);
-
+    arrays.languages.sort((a, b) => a - b);
+    
     numbersAction.onclick = () => {
         for (let number of arrays.numbers) {
             console.log(number);
             elfCode.appendToList(numbersDisplay, number);
+        }
+
+    }
+
+    languagesAction.onclick = () => {
+        for (let language of arrays.languages) {
+            console.log(language);
+            elfCode.appendToList(languagesDisplay, language);
         }
 
     }
