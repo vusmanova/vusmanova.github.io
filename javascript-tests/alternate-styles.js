b3a45875443accefa61094bd27d7b50fe4776501
Usmanova Visolakhon <visola.usmanova@gmail.com>
2019-12-08 13:42:14 -0800

    changes

--- javascript-tests/alternate-styles.js
+++ javascript-tests/alternate-styles.js
@@ -1,22 +1,27 @@
 window.onload = () => {
-
-    const appearanceButton = document.getElementById('appearanceAction');
-
     appearanceButton.onclick = () => {
-        const defaults = document.getElementById('default');
-        const basic = document.getElementById('basic');
+    const defaults = document.getElementById('default');
+    const basic = document.getElementById('basic');
+  const appearanceButton = document.getElementById('appearanceAction');
+  myForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const value = document.querySelector('input[name="switch"]:checked').value;
+    if (value.toLowerCase() === 'Default Stylesheet') {
+      disableStylesheet(defaults)
+    } else {
+      enableStylesheet(basic);
+        }
+  });
+}
+  
+      function disableStylesheet(node) {
+        node.rel = 'alternate stylesheet';
 
-        disableStylesheet(defaults);
-        enableStylesheet(basic);
+
+        function enableStylesheet(node) 
+        node.rel = 'stylesheet';
+
+      }
     }
 
-}
-
-function enableStylesheet(node) {
-    node.rel = 'stylesheet';
-}
-
-function disableStylesheet(node) {
-    node.rel = 'alternate stylesheet';
-}
 
--- javascript-tests/javascript-arrays.js
+++ javascript-tests/javascript-arrays.js
@@ -1,25 +1,79 @@
 window.onload = () => {
-
-    const elfCode = {
-        appendToList: (list, value) => {
-            const li = document.createElement("li");
-            li.appendChild(document.createTextNode(value));
-            list.appendChild(li);
-        }
-    }
-    const numbersAction = document.getElementById('numbers-action');
-    const numbersDisplay = document.getElementById('numbers-display');
-
-    const languagesAction = document.getElementById('languages-action');
-    const languagesDisplay = document.getElementById('languages-display');
-
+    
     const arrays = {
         numbers: [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5],
-        languages: ["JavaScript", "HTML", "CSS", "Java", "C#", "Python", "C/C++"]
+        languageNames: ["JavaScript", "HTML", "CSS", "Java", "C#", "Python", "C/C++"],
         languagePopularity: [
+            {
+                language: 'JavaScript',
+                rank: 1
+            },
+            {
+                language: 'HTML/CSS',
+                rank: 2
+
+            },
+            {
+                language: 'SQL',
+                rank: 3
+            },
+            {
+                language: 'Python',
+                rank: 4
+            },
+            {
+                language: 'Java',
+                rank: 5
+
+            },
+            {
+                language: 'C/C++',
+                rank: 6
+            }
 
         ]
+
+    }; 
+
+    const numbersAction = document.getElementById('numbers-action');
+    const languageNamesAction = document.getElementById('tech-language-names-action');
+    const techLanguagePopularityAction = document.getElementById('tech-language-popularity-action');
+    
+    numbersAction.onclick = () => {
+        const numbersDisplay = document.getElementById('numbers-display');
+        for (let number of arrays.numbers) {
+            const li = document.createElement("li");
+            li.appendChild(document.createTextNode(number));
+            numbersDisplay.appendChild(li);
+        }    
     };
+    languageNamesAction.onclick = () => {
+        const languageNamesDisplay = document.getElementById('tech-language-names-display');
+        for (let numberlanguageName of arrays.languageName) {
+            const li = document.createElement("li");
+            li.appendChild(document.createTextNode(number));
+            numbersDisplay.appendChild(li);
+     };
+    techLanguagePopularityAction.onclick = () => {};
+};  
+    
+    
+    const languagesNamesDisplay = document.getElementById('tech-language-names-display');
+    const numbersDisplay = document.getElementById('numbers-display');
+
+   
+        
+    
+        numbersAction.onclick = () => {
+            const numbersDisplay = document...;
+            for (let number of arrays.numbers) {
+                const li = document.createElement("li");
+                li.appendChild(document.createTextNode(number));
+                numbersDisplay.appendChild(li);
+            }    
+        };
+        languageNamesAction.onclick = () => {};
+        techLanguagePopularit
 
     arrays.numbers.sort((a, b) => a - b);
     arrays.languages.sort((a, b) => a - b);
@@ -37,6 +91,20 @@ window.onload = () => {
             console.log(language);
             elfCode.appendToList(languagesDisplay, language);
         }
-
+        arrays.languagePopularity.sort(compareRank);
+        // Code omitted here
+        
+        function compareRank(a, b) {
+            const rankA = a.rank;
+            const rankB = b.rank;
+        
+            let comparison = 0;
+            if (rankA > rankB) {
+                comparison = 1;
+            } else if (rankA < rankB) {
+                comparison = -1;
+            }
+            return comparison;
+        }
     }
 }
